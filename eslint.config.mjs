d2eb2e396f980751aa64bce9d2adff2fb95c003c
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-07-28 15:12:16 +0100

    chore: add root lint and prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,19 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import baseConfig from '../../eslint.config.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 export default [
-  ...baseConfig,
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
-    rules: {
-      'next/no-html-link-for-pages': 'off',
-    },
+    ignores: ['**/node_modules/**', '**/dist/**'],
   },
+  ...compat.extends('eslint:recommended'),
 ];
