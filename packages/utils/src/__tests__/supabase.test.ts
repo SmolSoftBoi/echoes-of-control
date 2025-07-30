444628f5f0ceee84ceb7b70b96e81340264e74a2
Kristian Matthews-Kennington <kristian@matthews-kennington.com>
2025-07-30 09:52:45 +0100

    test(utils): import vitest globals

--- packages/utils/src/__tests__/supabase.test.ts
+++ packages/utils/src/__tests__/supabase.test.ts
@@ -1,4 +1,4 @@
-import { vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { createClient } from '@supabase/supabase-js';
 import { createSupabaseClient } from '../supabase';
 
